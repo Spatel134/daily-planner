82ca2d1df1dd92d03a194af9d21d266eb659fad2
Sahil Patel <patelsaho@gmail.com>
2020-12-17 22:53:43 -0500

    Use jquery to add past, present, and future css properties to description

--- Develop/script.js
+++ Develop/script.js
@@ -1,26 +1,28 @@
+
 function generateSchedule() {
 
   var today = getDateNow();
+  // var hourNow = getHourNow();
+  var hourNow = 13;
   $("#currentDay").append(today);
 
 
   var timeArray = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
 
   for(var j = 0; j < 9; j++){
-    console.log("Enter j");
     if(localStorage.getItem(timeArray[j])==null){
-      console.log("clear");
       localStorage.setItem(timeArray[j], "");
     }
   }
 
-  for (var i = 0; i < 9; i++) {
-    var timeArea = $("<textArea disabled class='time-block hour col-1'>" + timeArray[i] + "</textArea>");
-    var descriptionArea = $("<textArea class='description col-10' id='"+ timeArray[i]+"'>"+localStorage.getItem(timeArray[i])+"</textArea>");
-    var saveButtonArea = $("<button class='saveBtn col-1' onclick='" +"callSaveDescription(\""+ timeArray[i]+"\")'></button>");
+  for (var i = 9; i < 18; i++) {
+    var timeArea = $("<textArea disabled class='time-block hour col-md-1'>" + timeArray[i-9] + "</textArea>");
+    var descriptionArea = $("<textArea class='description "+ "description" + i +" col-md-10' id='"+ timeArray[i-9]+"'>"+localStorage.getItem(timeArray[i-9])+"</textArea>");
+    var saveButtonArea = $("<button class='saveBtn col-md-1' onclick='" +"callSaveDescription(\""+ timeArray[i-9]+"\")'></button>");
     $(".container").append(timeArea);
     $(".container").append(descriptionArea);
     $(".container").append(saveButtonArea);
+    $(".description"+i).addClass(getPastPresentFuture(i,hourNow));
   }
 }
 
@@ -40,7 +42,19 @@ function getDateNow(){
   return moment().format('dddd, MMMM Do YYYY');
 }
 
-// function getPastPresentFuture(time){
-//   if()
+function getHourNow(){
+  return moment().hour();
+}
 
-// }
+function getPastPresentFuture(hour,hourNow){
+  
+  if(hour<hourNow){
+    return "past";
+  }
+  else if(hour>hourNow){
+    return "future";
+  }
+  else{
+    return "present";
+  }
+}
